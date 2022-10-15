e98cf1e60f4a852d1e19e2e9c3a082205a128f4b
John <joao.sj@estudante.iftm.edu.br>
2022-10-15 18:47:47 -0300

    Backend project created

---
+++ backend/src/main/java/com/adsiftm/adscatalog/AdscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.adsiftm.adscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adsiftm/adscatalog/AdscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsiftm.adscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
